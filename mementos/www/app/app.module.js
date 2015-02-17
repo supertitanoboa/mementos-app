d83815dfcdcbcc8b19a90c73dd697043eded9cde
Omar Duarte <omarduarte@gmail.com>
2015-02-16 16:35:20 -0800

    Adds app architecture
    Adds ionic starter application
    
    Refactors ionic starter application

---
+++ mementos/www/app/app.module.js
@@ -0,0 +1,30 @@
+angular.module('app', ['ionic', 'app.moment'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('moment', {
+    url: '/moment',
+    templateUrl: 'app/moment/moment.html',
+    controller: 'Moment',
+    controllerAs: 'vm'
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/moment');
+
+});
