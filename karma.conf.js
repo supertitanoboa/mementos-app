e4351a6a6d20b89a5c914ed27bba2dba26366415
Kiran Rao <k.ranrao@gmail.com>
2015-02-17 21:47:38 -0800

    Adds karma/jasmine testing framework
    Adds karma/jasmine testing framework
    
    Replaces chai unit test with karma unit test
    
    Replaces mocha with karma in gulpfile.  Adds gulp-karma as devDependency
    
    Adds karma/jasmine testing framework
    
    Edits gitignore to ignore coverage reports

---
+++ karma.conf.js
@@ -0,0 +1,79 @@
+// Karma configuration
+// Generated on Tue Feb 17 2015 21:57:05 GMT-0800 (PST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: './',
+
+    frameworks: ['jasmine', 'mocha', 'sinon', 'chai', 'chai-sinon'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      // lib files
+      'client/www/lib/angular/angular.js',
+      'client/www/lib/angular-animate/angular-animate.js',
+      'client/www/lib/angular-sanitize/angular-sanitize.js',
+      'client/www/lib/angular-ui-router/relase/angular-ui-router.js',
+      'client/www/lib/ionic/js/ionic-angular.js',
+      'client/www/lib/ionic/js/ionic-bundle.js',
+      'client/www/lib/ionic/js/ionic.js',
+      
+      // app
+      'client/www/app/**/*.js',
+
+      // all specs
+      'client/www/test/**/*.js'
+    ],
+
+    // list of files to exclude
+    exclude: [
+      'karma.conf.js'
+    ],
+
+    preprocessors: {
+      // Source files you want to generate coverage reports for
+      // This should not include tests or libraries
+      // These files will be instrumented by Istanbul
+      'client/www/app/**/*.js': ['coverage']
+    },
+
+    // progress reporter: lists each test run and whether they pass/fail
+    // coverage reporter: creates coverage reports for every tested browser
+    reporters: ['progress', 'coverage'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // start these browsers
+    browsers: ['Chrome'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    singleRun: false,
+
+    // report which specs run too slow
+    reportSlowerThan: 500,
+
+    // any additional plugins needed for testing
+    plugins: [
+      'karma-jasmine',
+      'karma-sinon',
+      'karma-chai-sinon',
+      'karma-coverage',
+      'karma-mocha',
+      'karma-chai',
+      'karma-chrome-launcher'
+    ]
+  });
+};
